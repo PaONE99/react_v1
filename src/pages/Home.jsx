bd3cd4fdec4131627025c14eb1838a8ffa8cf1aa
PaONE99 <pavan.jfsjd@gmail.com>
2024-09-09 14:15:47 +0530

    sample
    sample

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+         <div><h1>1</h1></div>
+         <div><h1>2</h1></div>
+         <div><h1>3</h1></div>
+    </div>
+  )
+}
+
+export default Home
